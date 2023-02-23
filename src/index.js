1c010591f4555c8beb5fa70f6473b6ac17ae3bb5
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-02-23 21:23:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,29 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1 className="heading">My Favourite Foods</h1>
-    <ul>
+    {/* <ul>
       <li>Bacon</li>
       <li>Jamon</li>
       <li>Noodles</li>
-    </ul>
+    </ul> */}
+    <div>
+      <img
+        className="f-img"
+        src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"
+        alt="abc"
+      />
+      <img
+        className="f-img"
+        src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"
+        alt="def"
+      />
+
+      <img
+        className="f-img"
+        src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"
+        alt="gho"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
